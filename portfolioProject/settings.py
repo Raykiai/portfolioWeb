5880b149d16c2ef3d69af827f52cffda25611b31
Rachael Kiai <rachaelnjerikiai1@gmail.com>
2022-07-01 15:08:38 +0300

    day 2

--- portfolioProject/settings.py
+++ portfolioProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolioProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'static/portfolioCS')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,10 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/portfolioCS/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/portfolioCS')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR,  'static'),  ]
 STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/portfolioCS'),
 ]
 
 # Default primary key field type
